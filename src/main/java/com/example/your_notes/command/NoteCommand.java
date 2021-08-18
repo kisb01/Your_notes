1b2e43aae44b4b0e6080709a6c683b088177e668
Bíró János Zoltán <kisb01@gmail.com>
2021-08-19 00:54:13 +0200

    Add Note Command

---
+++ src/main/java/com/example/your_notes/command/NoteCommand.java
@@ -0,0 +1,12 @@
+package com.example.your_notes.command;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NoteCommand {
+    private String message;
+}
