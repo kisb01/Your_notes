51e91fdd39bbe197ad6b98962ad3266ea9b5970e
Bíró János Zoltán <kisb01@gmail.com>
2021-08-19 00:46:11 +0200

    first commit

---
+++ src/main/java/com/example/your_notes/YourNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.your_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/your_notes/YourNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.your_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
